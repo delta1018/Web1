3b44295a1a6e78efb08fddcf737ac13daf84febd
delta1018 <cristhianospinacas@gmail.com>
2023-06-13 18:31:00 -0500

    initial commint

---
+++ script.js
@@ -0,0 +1,49 @@
+function showAlert() { alert("!Hola, esta es una alerta desde  Javascript"); }
+
+/** nav interaccion**/
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
+
+
+
+const miBoton = document.getElementById("miBoton");
+
+function handleClick(){
+    alert("Hola has hecho click en el boton");
+}
+
+miBoton.addEventListener("click",handleClick)
+
+console.log("hola mundo")
+
+
+
+const form = document.getElementById('myForm');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  validateForm();
+});
+
+
+
+
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+  }
+  function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+      alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+      alert('Correo electrónico enviado correctamente.');
+    }
+  }
+
+
+
+  
